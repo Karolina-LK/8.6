7bb021cd69aecf72871939c01d1d69a7b2e273ac
Karolina Lubieniecka <lubieniecka@interia.pl>
2017-08-02 12:24:58 +0200

    added edited files

--- 86.js
+++ 86.js
@@ -2,6 +2,15 @@ var a = 1,
     b = 2,
     value = (a*a)+(2*a*b)-(b*b);
 
+
 console.log('The value of the task is ' +value)
 
-if (value>0) {console.log ('Wynik dodatni')} else if (value<0) {console.log('Wynik ujemny')} else {console.log('Wynik równa się zero')}
+
+
+if (value>0) {
+	console.log ('Wynik dodatni')
+}  else if (value<0) {
+	console.log('Wynik ujemny')
+}  	else {
+	console.log('Wynik równa się zero')
+}
